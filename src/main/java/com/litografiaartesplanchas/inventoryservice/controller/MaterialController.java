58e4d12642d19ca9276ac79d53c3f46e326eb2d4
Juan-SCV <juanse.valencia200550@gmail.com>
2024-04-06 16:22:55 -0500

    LAP-149 Implementation

--- src/main/java/com/litografiaartesplanchas/inventoryservice/controller/MaterialController.java
+++ src/main/java/com/litografiaartesplanchas/inventoryservice/controller/MaterialController.java
@@ -2,14 +2,18 @@ package com.litografiaartesplanchas.inventoryservice.controller;
 
 import java.util.List;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.litografiaartesplanchas.inventoryservice.model.Material;
 import com.litografiaartesplanchas.inventoryservice.service.MaterialService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,14 +26,18 @@ public class MaterialController {
 
     @PostMapping("/save")
     public ResponseEntity<Object> saveMaterial(@RequestBody Material material) {
-        try {
-            ResponseEntity<Object> saveResult = materialService.saveMaterial(material); 
-            return saveResult;
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body("{\"status\": 400, \"message\": \"Something Went Wrong\"}");
-        }
+    try {
+        materialService.saveMaterial(material);
+        
+        return ResponseEntity.ok().body("{\"status\": 200, \"message\": \"Material saved successfully\"}");
+        
+    } catch (RuntimeErrorException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("{\"status\": 409, \"message\": \"Material already exists\"}");
+        
+    } catch (Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"status\": 400,\"message\": \"Something Went Wrong\"}");
     }
+}
 
     @GetMapping("/")
     public ResponseEntity<?> getAll(){
@@ -41,6 +49,16 @@ public class MaterialController {
             return ResponseEntity.ok(materials);
         } catch(Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"status\": 400,\"message\": \"Something Went Wrong\"}");
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Object> deleteMaterialById(@RequestParam("id") Integer id) {
+        try {
+            materialService.deleteMaterialById(id);
+            return ResponseEntity.ok().body("{\"status\": 200, \"message\": \"ok\"}");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"status\": 400, \"message\": \"Something Went Wrong\"}");
         }
     }
 }
