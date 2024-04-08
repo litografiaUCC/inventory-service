b45dca7eb92ceac03011021696ffdcf2b6462d27
Juan-SCV <juanse.valencia200550@gmail.com>
2024-04-08 16:46:08 -0500

    LAP-153 Implementation

--- src/main/java/com/litografiaartesplanchas/inventoryservice/service/MaterialService.java
+++ src/main/java/com/litografiaartesplanchas/inventoryservice/service/MaterialService.java
@@ -9,12 +9,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.litografiaartesplanchas.inventoryservice.model.Material;
+import com.litografiaartesplanchas.inventoryservice.model.TypeMaterial;
 import com.litografiaartesplanchas.inventoryservice.repository.MaterialRepository;
+import com.litografiaartesplanchas.inventoryservice.repository.TypeMaterialRepository;
 
 @Service
 public class MaterialService {
     @Autowired
     private MaterialRepository materialRepo;
+
+    @Autowired
+    private TypeMaterialRepository typeMaterialRepo;
 
     public void saveMaterial(Material material) {
         String materialName = material.getName();
@@ -48,13 +53,40 @@ public class MaterialService {
     public void updateMaterialQuantity(Integer id, int quantity) {
         Optional<Material> optionalMaterial = materialRepo.findById(id);
         
-        if (optionalMaterial.isPresent()) {
-            Material material = optionalMaterial.get();
-            material.setQuantity(quantity);
-
-            materialRepo.save(material);
-        } else {
+        if (optionalMaterial.isEmpty()) {
             throw new IllegalArgumentException("Material doesn't exist");
         }
+
+        Material material = optionalMaterial.get();
+        material.setQuantity(quantity);
+        materialRepo.save(material);
+    }
+
+    public void updateMaterial(Material updateMaterial) {
+        Optional<Material> optionalMaterial = materialRepo.findById((Integer) updateMaterial.getId());
+        if (optionalMaterial.isEmpty()) {
+            throw new IllegalArgumentException("Material doesn't exist");
+        }
+    
+        Material material = optionalMaterial.get();
+        
+        if (updateMaterial.getName() != null)  
+            material.setName(updateMaterial.getName());
+        
+        if (updateMaterial.getPrice() != 0) 
+            material.setPrice(updateMaterial.getPrice());
+        
+        if (updateMaterial.getQuantity() != 0) 
+            material.setQuantity(updateMaterial.getQuantity());
+    
+        if (updateMaterial.getTypeMaterial() != null) {
+            Optional<TypeMaterial> typeUpdate = typeMaterialRepo.findById((Integer) updateMaterial.getTypeMaterial().getId());
+            if (typeUpdate.isEmpty()) {
+                throw new IllegalArgumentException("Type Material invalid");
+            } else {
+                material.setTypeMaterial(typeUpdate.get());
+            }
+        }
+        materialRepo.save(material);
     }
 }  
